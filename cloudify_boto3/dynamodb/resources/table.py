9a06037003a2a7e0a941899477b16918b68f8c3d
earthmant <earthmant@icloud.com>
2017-05-08 13:43:44 +0300

    normalizing dynamo db

--- cloudify_boto3/dynamodb/resources/table.py
+++ cloudify_boto3/dynamodb/resources/table.py
@@ -12,11 +12,11 @@
 #    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
-'''
+"""
     DynamoDB.Table
     ~~~~~~~~~~~~~~
     AWS DynamoDB Table interface
-'''
+"""
 # Cloudify
 from cloudify_boto3.common import decorators, utils
 from cloudify_boto3.dynamodb import DynamoDBBase
@@ -24,19 +24,20 @@ from cloudify_boto3.dynamodb import DynamoDBBase
 from botocore.exceptions import ClientError
 
 RESOURCE_TYPE = 'DynamoDB Table'
+TABLE_NAME = 'TableName'
 
 
 class DynamoDBTable(DynamoDBBase):
-    '''
+    """
         AWS DynamoDB Table interface
-    '''
+    """
     def __init__(self, ctx_node, resource_id=None, client=None, logger=None):
         DynamoDBBase.__init__(self, ctx_node, resource_id, client, logger)
         self.type_name = RESOURCE_TYPE
 
     @property
     def properties(self):
-        '''Gets the properties of an external resource'''
+        """Gets the properties of an external resource"""
         resources = None
         try:
             resources = self.client.describe_table(
@@ -49,16 +50,16 @@ class DynamoDBTable(DynamoDBBase):
 
     @property
     def status(self):
-        '''Gets the status of an external resource'''
+        """Gets the status of an external resource"""
         props = self.properties
         if props and 'TableStatus' in props:
             return props['TableStatus']
         return None
 
     def create(self, params):
-        '''
+        """
             Create a new AWS DynamoDB Table.
-        '''
+        """
         self.logger.debug('Creating %s with parameters: %s'
                           % (self.type_name, params))
         res = self.client.create_table(**params)
@@ -67,11 +68,9 @@ class DynamoDBTable(DynamoDBBase):
         return self.resource_id, res['TableDescription']['TableArn']
 
     def delete(self, params=None):
-        '''
+        """
             Deletes an existing AWS DynamoDB Table.
-        '''
-        params = params or dict()
-        params.update(dict(TableName=self.resource_id))
+        """
         self.logger.debug('Deleting %s with parameters: %s'
                           % (self.type_name, params))
         self.client.delete_table(**params)
@@ -81,9 +80,13 @@ class DynamoDBTable(DynamoDBBase):
 @decorators.wait_for_status(status_pending=['CREATING', 'UPDATING'],
                             status_good=['ACTIVE'])
 def create(ctx, iface, resource_config, **_):
-    '''Creates an AWS DynamoDB Table'''
-    # Build API params
-    resource_config.update(dict(TableName=iface.resource_id))
+    """Creates an AWS DynamoDB Table"""
+
+    # Create a copy of the resource config for clean manipulation.
+    params = \
+        dict() if not resource_config else resource_config.copy()
+    params.update({TABLE_NAME: iface.resource_id})
+
     # Actually create the resource
     res_id, res_arn = iface.create(resource_config)
     utils.update_resource_id(ctx.instance, res_id)
@@ -94,5 +97,14 @@ def create(ctx, iface, resource_config, **_):
                          ignore_properties=True)
 @decorators.wait_for_delete(status_pending=['DELETING'])
 def delete(iface, resource_config, **_):
-    '''Deletes an AWS DynamoDB Table'''
-    iface.delete(resource_config)
+    """Deletes an AWS DynamoDB Table"""
+
+    # Create a copy of the resource config for clean manipulation.
+    params = \
+        dict() if not resource_config else resource_config.copy()
+
+    # Add the required TableName parameter.
+    if TABLE_NAME not in params.keys():
+        params.update({TABLE_NAME: iface.resource_id})
+
+    iface.delete(params)
