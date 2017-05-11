6a2d85e815669a68de762cf135d85be473706815
earthmant <earthmant@icloud.com>
2017-05-11 10:18:23 +0300

    fixing problems in setup.py

--- setup.py
+++ setup.py
@@ -12,41 +12,44 @@
 #    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
-'''Cloudify plugin package config'''
+"""Cloudify plugin package config"""
 
 from setuptools import setup
 
 
 setup(
     name='cloudify-boto3-plugin',
-    version='1.0.0m3',
+    version='1.0.0',
     license='LICENSE',
     packages=[
         'cloudify_boto3',
-        'cloudify_boto3.autoscaling',
-        'cloudify_boto3.autoscaling.resources'
         'cloudify_boto3.common',
+        'cloudify_boto3.autoscaling',
+        'cloudify_boto3.autoscaling.resources',
         'cloudify_boto3.cloudwatch',
-        'cloudify_boto3.cloudwatch.resources'
+        'cloudify_boto3.cloudwatch.resources',
         'cloudify_boto3.dynamodb',
         'cloudify_boto3.dynamodb.resources',
         'cloudify_boto3.ec2',
-        'cloudify_boto3.ec2.resources'
+        'cloudify_boto3.ec2.resources',
         'cloudify_boto3.elb',
-        'cloudify_boto3.elb.resources'
+        'cloudify_boto3.elb.resources',
+        'cloudify_boto3.elb.resources.classic',
         'cloudify_boto3.iam',
         'cloudify_boto3.iam.resources',
         'cloudify_boto3.kms',
-        'cloudify_boto3.kms.resources'
+        'cloudify_boto3.kms.resources',
         'cloudify_boto3.lambda_serverless',
         'cloudify_boto3.lambda_serverless.resources',
         'cloudify_boto3.rds',
         'cloudify_boto3.rds.resources',
         'cloudify_boto3.route53',
-        'cloudify_boto3.route53.resources'
+        'cloudify_boto3.route53.resources',
         'cloudify_boto3.s3',
-        'cloudify_boto3.s3.resources'
+        'cloudify_boto3.s3.resources',
         'cloudify_boto3.sns',
+        'cloudify_boto3.sns.resources',
+        'cloudify_boto3.sqs',
         'cloudify_boto3.sqs.resources'
     ],
     description='A Cloudify plugin for AWS',
