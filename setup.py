a725a73d069c539752a4ab65756dc03988ab8594
Mohammed AbuAisha <mohammeda@cloudify.co>
2018-03-12 11:39:18 +0200

    Add support for managing ebs volumes

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import find_packages
 
 setup(
     name='cloudify-awssdk-plugin',
-    version='2.0.0',
+    version='2.0.1',
     license='LICENSE',
     packages=find_packages(exclude=['tests*']),
     description='A Cloudify plugin for AWS',
