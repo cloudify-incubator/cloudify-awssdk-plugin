af8fa48a9b9ef31f384b985028e3e89a8ba5ef83
earthmant <trammell@cloudify.co>
2018-03-20 10:02:23 +0200

    changing v2.0.1 to be v2.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import find_packages
 
 setup(
     name='cloudify-awssdk-plugin',
-    version='2.0.1',
+    version='2.1.0',
     license='LICENSE',
     packages=find_packages(exclude=['tests*']),
     description='A Cloudify plugin for AWS',
